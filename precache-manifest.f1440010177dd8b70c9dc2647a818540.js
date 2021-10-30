55b53039dd4191cbc5576950fdb0fe77d93a2e36
hank fuller <fuller120@gmail.com>
2021-10-30 07:03:22 -0400

    Updates

---
+++ precache-manifest.f1440010177dd8b70c9dc2647a818540.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478ff245ef89bb2b96452f8b2b4b6386",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a556394d74370edfc7b",
+    "url": "./static/css/2.afc6d123.chunk.css"
+  },
+  {
+    "revision": "ced6e860d3c9f079d7ce",
+    "url": "./static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "5a556394d74370edfc7b",
+    "url": "./static/js/2.e261778f.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "./static/js/2.e261778f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced6e860d3c9f079d7ce",
+    "url": "./static/js/main.f1a494d6.chunk.js"
+  },
+  {
+    "revision": "e720711dd3e4e8b209d0",
+    "url": "./static/js/runtime-main.43c7aded.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "./static/media/box.df8b84d5.png"
+  }
+]);
